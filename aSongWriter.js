8add6960f59e74f0c7db882daa712c1eae76b5e8
Zachary Biernat <zackbiernat@gmail.com>
2017-08-25 14:00:02 -0700

    Added file for aSongWriter

---
+++ aSongWriter.js
@@ -0,0 +1,94 @@
+//Song writer function
+/*Conventionality of (1-3)
+  time signature(s),
+  key (key changes),
+  rhythm,
+  progression (chord inversions),
+  and melody
+  as inputs to the function
+  */
+
+//Song constructor
+var Song = function (timeSig, key, rhythm, progression) {
+
+}
+
+Song.prototype.timeSig = function (timeSig) {
+  switch (timeSig) {
+    case 1:
+      //simple
+      [break;]
+    case 2:
+      //less simple
+       break;]
+
+    case 3:
+      //complex
+      [break;]
+
+  }
+}
+
+Song.prototype.key = function(key) {
+  switch (key) {
+    case 1:
+      //simple
+      [break;]
+    case 2:
+      //less simple
+       break;]
+
+    case 3:
+      //complex
+      [break;]
+  }
+}
+
+Song.prototype.rhythm = function(rhythm) {
+  switch (rhythm) {
+    case 1:
+      //simple
+      [break;]
+    case 2:
+      //less simple
+       break;]
+
+    case 3:
+      //complex
+      [break;]
+
+  }
+}
+
+Song.prototype.progression = function(progression) {
+  switch (progression) {
+    case 1:
+      //simple
+      [break;]
+    case 2:
+      //less simple
+       break;]
+
+    case 3:
+      //complex
+      [break;]
+
+  }
+
+}
+
+Song.prototype.melody = function(melody) {
+  switch (melody) {
+    case 1:
+      //simple
+      [break;]
+    case 2:
+      //less simple
+       break;]
+
+    case 3:
+      //complex
+      [break;]
+
+  }
+}
