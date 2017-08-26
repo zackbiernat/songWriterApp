5e49b1a22599a70b4ab2fca1267189740bddd1be
Zachary Biernat <zackbiernat@gmail.com>
2017-08-25 18:06:17 -0700

    "Fixed some stupid things that 'merge' inserted'

--- aSongWriter.js
+++ aSongWriter.js
@@ -9,7 +9,6 @@
   */
 
 //Song constructor
-<<<<<<< HEAD
 var Song = function () {
   // timeSig;//varies with complexity 1-3
   // key;//accepts major, minor, or atonal
@@ -41,6 +40,7 @@ Song.prototype.timeSigGen = function (timeSig) {
 
       default: console.log('You did not enter a valid value!');
   }
+
   switch (output) {
     case 0:
     this.timeSig= '4/4';
@@ -65,32 +65,11 @@ Song.prototype.timeSigGen = function (timeSig) {
     case 5:
     this.timeSig= '5/4';
     break;
-
-=======
-var Song = function (timeSig, key, rhythm, progression) {
-
-}
-
-Song.prototype.timeSig = function (timeSig) {
-  switch (timeSig) {
-    case 1:
-      //simple
-      [break;]
-    case 2:
-      //less simple
-       break;]
-
-    case 3:
-      //complex
-      [break;]
->>>>>>> 8add6960f59e74f0c7db882daa712c1eae76b5e8
-
   }
-}
+};
 
 Song.prototype.key = function(key) {
   switch (key) {
-<<<<<<< HEAD
     case 'major':
       //major
       return 'major';
@@ -106,23 +85,9 @@ Song.prototype.key = function(key) {
       break;
   }
 }
-/*
-=======
-    case 1:
-      //simple
-      [break;]
-    case 2:
-      //less simple
-       break;]
 
-    case 3:
-      //complex
-      [break;]
-  }
-}
 
->>>>>>> 8add6960f59e74f0c7db882daa712c1eae76b5e8
-Song.prototype.rhythm = function(rhythm) {
+/*Song.prototype.rhythm = function(rhythm) {
   switch (rhythm) {
     case 1:
       //simple
@@ -137,16 +102,13 @@ Song.prototype.rhythm = function(rhythm) {
 
   }
 }
-<<<<<<< HEAD
 */
-=======
->>>>>>> 8add6960f59e74f0c7db882daa712c1eae76b5e8
+
 
 Song.prototype.progression = function(progression) {
   switch (progression) {
     case 1:
       //simple
-<<<<<<< HEAD
       break;
     case 2:
       //less simple
@@ -155,17 +117,6 @@ Song.prototype.progression = function(progression) {
     case 3:
       //complex
       break;
-=======
-      [break;]
-    case 2:
-      //less simple
-       break;]
-
-    case 3:
-      //complex
-      [break;]
->>>>>>> 8add6960f59e74f0c7db882daa712c1eae76b5e8
-
   }
 
 }
@@ -174,25 +125,15 @@ Song.prototype.melody = function(melody) {
   switch (melody) {
     case 1:
       //simple
-<<<<<<< HEAD
       break;
     case 2:
       //less simple
        break;
-
     case 3:
       //complex
       break;
-=======
-      [break;]
-    case 2:
-      //less simple
-       break;]
+    }
 
-    case 3:
-      //complex
-      [break;]
->>>>>>> 8add6960f59e74f0c7db882daa712c1eae76b5e8
 
   }
 }
